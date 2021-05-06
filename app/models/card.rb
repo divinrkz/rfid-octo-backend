67cc5c6be2ce2404827b637b924c5c87cd551f44
Divin Irakiza <irakizadivin@gmail.com>
2021-05-06 16:05:01 +0200

    ORM Relations between models

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,4 +1,5 @@
 class Card < ApplicationRecord
   validates :uuid, presence: true
   validates :balance, presence: true
+  has_many :transactions
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,4 +1,5 @@
 class Transaction < ApplicationRecord
   validates :card, presence: true
   validates :fare, presence: true
+  belongs_to :card
 end
