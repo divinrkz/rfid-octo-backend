0fd380e2dd77ac1ed4d252a2413ec93bc917c09e
Divin Irakiza <irakizadivin@gmail.com>
2021-05-07 05:01:22 +0200

    Card HasMany Transaction relaation

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,5 +1,6 @@
 class Card < ApplicationRecord
   validates :uuid, presence: true
   validates :balance, presence: true
-  has_many :transactions
+
+  has_many :transactions, class_name: 'Transaction', foreign_key: 'id'
 end
