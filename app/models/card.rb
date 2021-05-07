870a7a8b0ee69f50ed0dcd04002d61da427f3b1a
Divin Irakiza <irakizadivin@gmail.com>
2021-05-07 21:16:58 +0200

    Re-migrated data

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,2 +1,6 @@
 class Card < ApplicationRecord
+  validates :uuid, presence: true
+  validates :balance, presence: true
+
+  has_many :transactions, class_name: 'Transaction', foreign_key: 'id'
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,2 +1,6 @@
 class Transaction < ApplicationRecord
+  validates :card, presence: true
+  validates :fare, presence: true
+
+  belongs_to :card, class_name: 'Card'
 end
